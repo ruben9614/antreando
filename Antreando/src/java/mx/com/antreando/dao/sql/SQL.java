f64c6c4d1ee587f32eb51b59c7833236c9c7831a
ruben.arizmendi88@gmail.com <ruben.arizmendi88@gmail.com@65c36f39-f316-f5e6-0f46-c85ede313b9a>
2011-09-27 03:34:57 +0000

    se agrego codigo a SQL

--- Antreando/src/java/mx/com/antreando/dao/sql/SQL.java
+++ Antreando/src/java/mx/com/antreando/dao/sql/SQL.java
@@ -10,4 +10,59 @@ package mx.com.antreando.dao.sql;
  */
 public class SQL {
     
+    //TcCategoria
+    public static String tcCategoriaSelectAll="";
+    public static String tcCategoriaSelectById="";
+    public static String tcCategoriaInsert="";
+    public static String tcCategoriaUpdateById="";
+    public static String tcCategoriaDeleteById="";
+    
+    //TcPromociones
+    public static String tcPromocionesSelectAll="";
+    public static String tcPromocionesSelectById="";
+    public static String tcPromocionesInsert="";
+    public static String tcPromocionesUpdateById="";
+    public static String tcPromocionesDeleteById="";
+    
+    //TcTipoUsuario
+    public static String tcTipoUsuarioSelectAll="";
+    public static String tcTipoUsuarioSelectById="";
+    public static String tcTipoUsuaioInsert="";
+    public static String tcTipoUsuaioUpdateById="";
+    public static String tcTipoUsuaioDeleteById="";
+    
+    //TcZona
+    public static String tcZonaSelectAll="";
+    public static String tcZonaSelectById="";
+    public static String tcZonaInsert="";
+    public static String tcZonaUpdateById="";
+    public static String tcZonaDeleteById="";
+    
+    //TrAntro
+    public static String trAntroSelectAll="";
+    public static String trAntroSelectById="";
+    public static String trAntroInsert="";
+    public static String trAntroUpdateById="";
+    public static String trAntroDeleteById="";
+    
+    //TrNoticias
+    public static String trNoticiasSelectAll="";
+    public static String trNoticiasSelectById="";
+    public static String trNoticiasInsert="";
+    public static String trNoticiasUpdateById="";
+    public static String trNoticiasDeleteById="";
+    
+    //TrReservaciones
+    public static String trReservacionesSelectAll="";
+    public static String treservacionesSelectById="";
+    public static String trReservacionesInsert="";
+    public static String trReservacionesUpdateById="";
+    public static String trReservacionesDeleteById="";
+    
+    //TrUsuarios
+    public static String trUsuariosSelectAll="";
+    public static String trUsuariosSelectById="";
+    public static String trUsuariosInsert="";
+    public static String trUsuariosUpdateById="";
+    public static String trUsuariosDeleteById="";
 }
